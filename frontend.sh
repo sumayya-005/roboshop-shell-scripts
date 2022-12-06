23fe63e9f8b6b7e28aa6f3dbd2aed8d88e8bb44a
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-12-06 23:43:46 +0530

    updating ./roboshop-shell-scripts

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,27 @@
+Log_File=tmp/frontend
+
  echo Installing Nginx
- yum install nginx -y &>>/tmp/frontend
+ yum install nginx -y &>>$Log_File
  echo Status $?
 
  echo Downloading the webcontent
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$Log_File
  echo Status $?
 
- cd /usr/share/nginx/html &>>/tmp/frontend
+ cd /usr/share/nginx/html &>>$Log_File
 
  echo Removing the old webcontent
- rm -rf * &>>/tmp/frontend
- unzip /tmp/frontend.zip &>>/tmp/frontend
+ rm -rf * &>>$Log_File
+ unzip /tmp/frontend.zip &>>$Log_File
  echo Status $?
 
  echo Moving the frontend-main
- mv frontend-main/static/* . &>>/tmp/frontend
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+ mv frontend-main/static/* . &>>$Log_File
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$Log_File
  echo Status $?
 
  echo Restarting Nginx
- systemctl restart nginx &>>/tmp/frontend
- systemctl enable nginx &>>/tmp/frontend
- systemctl start nginx &>>/tmp/frontend
+ systemctl restart nginx &>>$Log_File
+ systemctl enable nginx &>>$Log_File
+
  echo Status $?
---
+++ mongodb.sh
@@ -0,0 +1,38 @@
+Log_File=/tmp/mongo
+echo "Setup Mongodb Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$Log_File
+echo Status=$?
+
+
+ echo "Installing Mongodb"
+ yum install -y mongodb-org &>>$Log_File
+echo Status=$?
+
+ echo "Update the Listen Address"
+ sed -i -e '/s/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$Log_File
+
+ echo "Starting Mongodb "
+ systemctl enable mongod &>>$Log_File
+ systemctl restart mongod &>>$Log_File
+echo Status=$?
+
+echo "Download the Mongodb Schema files"
+ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$Log_File
+echo Status=$?
+
+
+ cd /tmp &>>$Log_File
+
+ echo "Extract the files"
+ unzip mongodb.zip &>>$Log_File
+ cd mongodb-main &>>$Log_File
+ echo Status=$?
+
+ echo "Load the Catalogue schema"
+ mongo < catalogue.js &>>$Log_File
+ echo Status=$?
+
+ echo "Load the User schema"
+ mongo < users.js &>>$Log_File
+ echo Status=$?
+
