66137d8d1a607be2c85caf3b9b270392e5d12cb4
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-12-07 14:52:37 +0530

    updating ./roboshop-shell-scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,37 @@
 Log_File=/tmp/catalogue
 
+
 echo "Setup Catalogue NodeJs"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+ 
+fi
 
 echo "Installing Nodejs "
 yum install nodejs -y &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
 echo "Add Roboshop Username"
 useradd roboshop &>>$Log_File
-echo status=$?
-
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 echo "Download the schema"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
 cd /home/roboshop
 
@@ -22,19 +39,35 @@ echo "Extract the Schema"
 unzip /tmp/catalogue.zip &>>$Log_File
 mv catalogue-main catalogue &>>$Log_File
 cd /home/roboshop/catalogue &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
 echo "Install the NodeJs Dependencies"
 npm install &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
 echo "Setup the Catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
 systemctl daemon-reload &>>$Log_File
 systemctl enable catalogue &>>$Log_File
 
 echo "Start the Service"
 systemctl start catalogue &>>$Log_File
-echo status=$?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,46 @@ Log_File=/tmp/frontend
 
  echo Installing Nginx
  yum install nginx -y &>>$Log_File
- echo Status $?
+ if [ $? -eq 0 ];
+   echo status=SUCESS
+ then
+   echo status=FAILURE
+ fi
 
  echo Downloading the webcontent
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$Log_File
- echo Status $?
+ if [ $? -eq 0 ];
+   echo status=SUCESS
+ then
+   echo status=FAILURE
+ fi
 
  cd /usr/share/nginx/html &>>$Log_File
 
  echo Removing the old webcontent
  rm -rf * &>>$Log_File
  unzip /tmp/frontend.zip &>>$Log_File
- echo Status $?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
 
  echo Moving the frontend-main
  mv frontend-main/static/* . &>>$Log_File
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$Log_File
- echo Status $?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
+
 
  echo Restarting Nginx
  systemctl restart nginx &>>$Log_File
  systemctl enable nginx &>>$Log_File
-
- echo Status $?
+if [ $? -eq 0 ];
+  echo status=SUCESS
+then
+  echo status=FAILURE
+fi
