35933eee0f6f84832feb904c7eedcdd9cb8e5926
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-12-07 16:14:53 +0530

    updating ./roboshop-shell-scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,19 @@
 Log_File=/tmp/catalogue
 
-source=common.sh
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo you should run this script with a sudo privilages or root useradd
+  exit
+fi
+
+StatusCheck() {
+  if [ $1 -eq 0 ]; then
+    echo -e Status = "\e[32mSUCCESS\e[0m"
+  else
+    echo -e Status = "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
+}
 
 echo "Setup Catalogue NodeJs"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$Log_File
--- common.sh
+++ common.sh
@@ -1,14 +1,14 @@
-ID=$(id -u)
-if [ $ID -ne 0 ]; then
-  echo you should run this script with a sudo privilages or root useradd
-  exit
-fi
-
-StatusCheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-    echo -e Status = "\e[31mFAILURE\e[0m"
-    exit 1
-  fi
-}
+#ID=$(id -u)
+#if [ $ID -ne 0 ]; then
+#  echo you should run this script with a sudo privilages or root useradd
+#  exit
+#fi
+#
+#StatusCheck() {
+#  if [ $1 -eq 0 ]; then
+#    echo -e Status = "\e[32mSUCCESS\e[0m"
+#  else
+#    echo -e Status = "\e[31mFAILURE\e[0m"
+#    exit 1
+#  fi
+#}
