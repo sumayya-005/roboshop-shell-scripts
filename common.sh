7d6283a30fac4fc3b60d5838962fd273661fdb09
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-12-07 16:12:20 +0530

    updating ./roboshop-shell-scripts

--- common.sh
+++ common.sh
@@ -5,10 +5,10 @@ if [ $ID -ne 0 ]; then
 fi
 
 StatusCheck() {
- if [ $1 -eq 0 ];then
-   echo -e status="\e[32mSUCCESS\e[0m"
- else
-   echo -e status="\e[35mFAILURE\e[0m"
- exit 1
- fi
+  if [ $1 -eq 0 ]; then
+    echo -e Status = "\e[32mSUCCESS\e[0m"
+  else
+    echo -e Status = "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
 }
